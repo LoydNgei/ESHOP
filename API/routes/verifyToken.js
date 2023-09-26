69d93aa655cef9e7b7657ac3b4063c13f5be8855
Loyd Ngei <loydngei@gmail.com>
2023-09-26 07:37:50 +0300

    Eshop

---
+++ API/routes/verifyToken.js
@@ -0,0 +1,55 @@
+// Admin/User registers or logs in and receives a JWT.
+
+// Admin/User includes the JWT in the request header when trying to post products.
+
+// The verifyToken middleware verifies the JWT and extracts the isAdmin field.
+
+// The verifyTokenAndAdmin middleware checks if the user is an admin based on the isAdmin field.
+
+// If the user is an admin, they are allowed to do Admin stuff.
+
+
+
+
+
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user._id === req.params._id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
